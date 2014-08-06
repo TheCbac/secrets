aea649cb928e23387d85df46d86bd121b85040a1
Craig Cheney <ccheney@mit.edu>
2014-08-05 23:30:07 -0400

    reshape some stuff

--- router.js
+++ router.js
@@ -6,11 +6,33 @@ Router.map(function(){
 
 	this.route('01a_lettersHint_link',{
 		path:'/01a_lettersHint',
-		template:'01a_lettersClue_tmpl'
+		template:'01a_lettersHint_tmpl'
 	});
 
 	this.route('02_comments_link',{
 		path:'/comments',
 		template:'02_comments_tmpl'
 	});
+
+
+	// // for rendering '404'
+	// this.route('regionEdit', {
+	// 	path: '/region/:_id',
+	// 	waitOn: function() {
+	// 		return Meteor.subscribe('region', this.params._id);
+	// 	},
+	// 	data: function() {
+	// 		var region = Regions.findOne({
+	// 			_id: this.params._id
+	// 		});
+	// 		if(!region)
+	// 			this.render("notFound");
+	// 		else
+	// 			return region;
+	// 	}
+	// });
+
+	
 });
+
+
--- secrets.js
+++ secrets.js
@@ -1,15 +1,21 @@
 if (Meteor.isClient) {
-  Template.hello.greeting = function () {
-    return "Welcome to secrets.";
-  };
+  // Template.hello.greeting = function () {
+  //   return "Welcome to secrets.";
+  // };
 
-  Template.hello.events({
-    'click input': function () {
-      // template data, if any, is available in 'this'
-      if (typeof console !== 'undefined')
-        console.log("You pressed the button, Brian");
-    }
-  });
+  // Template.hello.events({
+  //   'click input': function () {
+  //     // template data, if any, is available in 'this'
+  //     if (typeof console !== 'undefined')
+  //       console.log("You pressed the button, Brian");
+  //   }
+  // });
+
+  // Template.01_letters_tmpl.events({
+  //   'click input': function () {
+  //     console.log("hello");
+  //   }
+  // });
 }
 
 if (Meteor.isServer) {
